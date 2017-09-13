c2d036ed31745ed126de7745b2f834e7512f697e
Michael Truog <mjtruog@gmail.com>
2017-09-12 18:56:49 -0700

    Version 1.7.2.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceQueue do
 
   def project do
     [app: :cloudi_service_queue,
-     version: "1.7.1",
+     version: "1.7.2",
      language: :erlang,
      description: description(),
      package: package(),
@@ -14,7 +14,7 @@ defmodule CloudIServiceQueue do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.7.1"}]
+    [{:cloudi_core, "~> 1.7.2"}]
   end
 
   defp description do
