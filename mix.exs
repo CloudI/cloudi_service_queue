7fcde0f4728be0f7369150871f5f0bb706a81fb3
Michael Truog <mjtruog@protonmail.com>
2020-06-03 21:43:24 -0700

    Version 2.0.0.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceQueue do
 
   def project do
     [app: :cloudi_service_queue,
-     version: "1.8.0",
+     version: "2.0.0",
      language: :erlang,
      erlc_options: [
        :debug_info,
@@ -29,7 +29,7 @@ defmodule CloudIServiceQueue do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.8.0"}]
+    [{:cloudi_core, "~> 2.0.0"}]
   end
 
   defp description do
