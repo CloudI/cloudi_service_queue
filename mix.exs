6d5008f0e2c25227fc351915aeb0271802c31d1e
Michael Truog <mjtruog@protonmail.com>
2020-11-26 19:36:08 -0800

    Version 2.0.1.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceQueue do
 
   def project do
     [app: :cloudi_service_queue,
-     version: "2.0.0",
+     version: "2.0.1",
      language: :erlang,
      erlc_options: [
        :debug_info,
@@ -29,7 +29,7 @@ defmodule CloudIServiceQueue do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 2.0.0"}]
+    [{:cloudi_core, "~> 2.0.1"}]
   end
 
   defp description do
@@ -37,7 +37,7 @@ defmodule CloudIServiceQueue do
   end
 
   defp package do
-    [files: ~w(src doc test rebar.config README.markdown),
+    [files: ~w(src doc test rebar.config README.markdown LICENSE),
      maintainers: ["Michael Truog"],
      licenses: ["MIT"],
      links: %{"Website" => "https://cloudi.org",
